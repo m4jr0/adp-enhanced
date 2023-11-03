a748057c6a2cbe5fb80c66e9e0d3503cb2aac751
m4jr0 <lowgravityisfun@gmail.com>
2023-11-03 16:44:04 +0100

    Fix script links

--- src/adp_enhanced.user.js
+++ src/adp_enhanced.user.js
@@ -1,20 +1,23 @@
 // ==UserScript==
 // @name         ADP Enhanced
+// @namespace    https://github.com/m4jr0/adp-enhanced
+// @downloadURL  https://raw.githubusercontent.com/m4jr0/adp-enhanced/master/src/adp_enhanced.user.js
+// @updateURL    https://raw.githubusercontent.com/m4jr0/adp-enhanced/master/src/adp_enhanced.user.js
 // @version      0.9.1
 // @description  Enhance ADP
 // @author       m4jr0
 // @match        https://mon.adp.com/redbox/*/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=adp.com
 // @grant        none
-// @require      https://github.com/m4jr0/adp-enhanced/tree/master/src/global.js
-// @require      https://github.com/m4jr0/adp-enhanced/tree/master/src/utils.js
-// @require      https://github.com/m4jr0/adp-enhanced/tree/master/src/date_utils.js
-// @require      https://github.com/m4jr0/adp-enhanced/tree/master/src/dom_utils.js
-// @require      https://github.com/m4jr0/adp-enhanced/tree/master/src/settings.js
-// @require      https://github.com/m4jr0/adp-enhanced/tree/master/src/adp_api.js
-// @require      https://github.com/m4jr0/adp-enhanced/tree/master/src/css.js
-// @require      https://github.com/m4jr0/adp-enhanced/tree/master/src/calendar.js
-// @require      https://github.com/m4jr0/adp-enhanced/tree/master/src/loader.js
+// @require      https://github.com/m4jr0/adp-enhanced/raw/master/src/global.js
+// @require      https://github.com/m4jr0/adp-enhanced/raw/master/src/utils.js
+// @require      https://github.com/m4jr0/adp-enhanced/raw/master/src/date_utils.js
+// @require      https://github.com/m4jr0/adp-enhanced/raw/master/src/dom_utils.js
+// @require      https://github.com/m4jr0/adp-enhanced/raw/master/src/settings.js
+// @require      https://github.com/m4jr0/adp-enhanced/raw/master/src/adp_api.js
+// @require      https://github.com/m4jr0/adp-enhanced/raw/master/src/css.js
+// @require      https://github.com/m4jr0/adp-enhanced/raw/master/src/calendar.js
+// @require      https://github.com/m4jr0/adp-enhanced/raw/master/src/loader.js
 // ==/UserScript==
 
 /* global GM_info */
